8a90126f38a12a5e967603277b0b428e13092992
YUNHO KIM <yh900630@gmail.com>
2020-07-16 10:50:57 +0900

    ユーザ登録エラー

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 
-// hamada テスト2
 class UserController extends Controller
 {
 
@@ -18,23 +17,22 @@ class UserController extends Controller
 
     public function userCreate(Request $request)
     {
-        // $user = User::create($request->user);
-        // return response()->json(['user' => $user]);
+        $user = new User();
+            
         if (request()->img_path) {
             $file_name = time() . '.' . request()->img_path->getClientOriginalName();
             request()->img_path->storeAs('public/image', $file_name);
- 
-            $user = new User();
             $user->img_path = 'image/' . $file_name;
+        }
             $user->name = $request->name;
             $user->email = $request->email;
             $user->password = $request->password;
             $user->se_career = $request->se_career;
             $user->introduction = $request->introduction;
             $user->save();
- 
-            return ['success' => '登録しました!'];
-        }
+
+        return ['success' => '登録しました!'];
+            
     }
 
     public function userDetail(User $user)
