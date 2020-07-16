8e4197416d00b1b89f113cfbacb4b028763fa452
Kazutaka Hamada <hama0102105@gmail.com>
2020-07-16 10:38:03 +0900

    List,Edit画面を編集
    List,Edit画面を編集

--- src/resources/js/components/UserEditComponent.vue
+++ src/resources/js/components/UserEditComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div>
+    <!-- 次回のバージョンアップでは認証機能を導入する予定。その時に以下を使う可能性があるのでコメントアウトで残しておきます。 hamada-->
+    <!-- 不必要なら削除してOKです hamada -->
+    <!-- <div>
       <h1>ユーザ更新</h1>
       <p>User Id: {{ user.id }}</p>
       <form @submit.prevent="updateUser">
@@ -14,7 +16,41 @@
         </div>
         <button type="submit">更新</button>
       </form>
-    </div>
+    </div> -->
+      <div class="form-row">
+        <div class="user-form-left">
+
+          <div class="user-form-left-top">
+              <!-- 氏名フォーム -->
+              <div class="form-group" id="name-form">
+                <label for="name">氏名</label>
+                <input v-model="user.name" type="text" class="form-control" placeholder="氏名" readonly>
+              </div>
+
+              <!-- エンジニア歴フォーム -->
+              <div class="form-group col-md-4" id="se-career-form">
+                <label for="se_career">エンジニア歴</label>
+                <input v-model="user.se_career" type="text" class="form-control" readonly>
+              </div>
+
+          </div>
+
+            <!-- 自己紹介フォーム -->
+            <div class="form-group" id="introduction-form">
+              <label for="introduction">自己紹介</label>
+              <textarea v-model="user.introduction" class="form-control" rows="4" readonly></textarea>
+            </div>
+
+        </div>
+
+        <div class="user-form-right">
+          <label for="inputEmail4">アイコン</label>
+          
+          <img v-bind:src="'/storage/'+user.img_path" alt="画像はありません" id="user-image">
+        </div>
+
+      </div> 
+
   </div>
 </template>
 
@@ -26,7 +62,10 @@ export default {
       user: {
         id: "",
         name: "",
-        email: ""
+        email: "",
+        se_career:"",
+        introduction:"",
+        img_path:""
       }
     };
   },
